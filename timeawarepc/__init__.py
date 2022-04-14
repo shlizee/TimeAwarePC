f362d59a77f109a49c643a3f56e49cab17cd5ed7
Rahul Biswas <rbiswas1@uw.edu>
2022-04-13 17:47:11 -0700

    Update __init__.py

---
+++ timeawarepc/__init__.py
@@ -0,0 +1,6 @@
+"""
+Time-Aware PC Algorithm for estimating Causal Functional Connectivity from Time Series
+"""
+
+__version__ = "0.0.1"
+__all__ = ["tpc_helpers","tpc","find_cfc","pcalg","pcalg_helpers","simulate_data"]
