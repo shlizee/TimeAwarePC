69822695e8d6ac00c18e19ea2457251085760abc
Rahul Biswas <rbiswas1@uw.edu>
2022-04-15 12:18:55 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open('README.md','r') as fh:
     README = fh.read()
 
-VERSION = "1.0.3"
+VERSION = "1.0.5"
 
 setuptools.setup(
     # Needed to silence warnings (and to be a worthwhile package)
